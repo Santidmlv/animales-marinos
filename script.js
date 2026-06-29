272de9ef7ac994d0236aa4c796e2a675e9e4dbf5
Santidmlv <43052109+Santidmlv@users.noreply.github.com>
2026-06-28 21:30:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,57 @@ const animales = {
 
     tortuga: {
         nombre: "TORTUGA MARINA",
-        imagen: "imagenes/tortuga.png"
+        imagen: "imagenes/tortuga.jpeg"
     },
 
     pulpo: {
         nombre: "PULPO",
-        imagen: "imagenes/pulpo.png"
+        imagen: "imagenes/pulpo.jpeg"
     },
 
     tiburon: {
         nombre: "TIBURÓN",
-        imagen: "imagenes/tiburon.png"
+        imagen: "imagenes/tiburon.jpeg"
+    },
+
+    ballena: {
+        nombre: "BALLENA",
+        imagen: "imagenes/ballena.jpeg"
+    },
+
+    caballito: {
+        nombre: "CABALLITO DE MAR",
+        imagen: "imagenes/caballito.jpeg"
+    },
+
+    cangrejo: {
+        nombre: "CANGREJO",
+        imagen: "imagenes/cangrejo.jpeg"
+    },
+
+    estrella: {
+        nombre: "ESTRELLA DE MAR",
+        imagen: "imagenes/estrella.jpeg"
+    },
+
+    pez: {
+        nombre: "PEZ",
+        imagen: "imagenes/pez.jpeg"
+    },
+
+    pezespada: {
+        nombre: "PEZ ESPADA",
+        imagen: "imagenes/pezespada.jpeg"
+    },
+
+    pezpayaso: {
+        nombre: "PEZ PAYASO",
+        imagen: "imagenes/pezpayaso.jpeg"
+    },
+
+    pezglobo: {
+        nombre: "PEZ GLOBO",
+        imagen: "imagenes/pezglobo.jpeg"
     }
 
 };
