66b72cedc9964f588151c1fe44b7d5dfc543dfd9
Santidmlv <43052109+Santidmlv@users.noreply.github.com>
2026-06-28 21:01:27 -0300

    Add animal data and display logic in script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const animales = {
+
+    delfin: {
+        nombre: "DELFIN",
+        imagen: "imagenes/delfin.png"
+    },
+
+    tortuga: {
+        nombre: "TORTUGA MARINA",
+        imagen: "imagenes/tortuga.png"
+    },
+
+    pulpo: {
+        nombre: "PULPO",
+        imagen: "imagenes/pulpo.png"
+    },
+
+    tiburon: {
+        nombre: "TIBURÓN",
+        imagen: "imagenes/tiburon.png"
+    }
+
+};
+
+
+const parametros = new URLSearchParams(window.location.search);
+
+const elegido = parametros.get("animal");
+
+
+const imagen = document.getElementById("animal");
+const nombre = document.getElementById("nombre");
+
+
+if (animales[elegido]) {
+
+    imagen.src = animales[elegido].imagen;
+    imagen.alt = animales[elegido].nombre;
+
+    nombre.textContent = animales[elegido].nombre;
+
+} else {
+
+    nombre.textContent = "ANIMAL DEL MAR";
+
+    imagen.src = "imagenes/delfin.png";
+
+}
